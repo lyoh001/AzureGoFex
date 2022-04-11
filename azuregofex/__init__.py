5a3181f1cd94d851202e70113947a77b4bbb5637
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-04-11 13:30:31 +1000

    Commit

--- azuregofex/__init__.py
+++ azuregofex/__init__.py
@@ -1,103 +1,106 @@
-# import asyncio
-# import base64
-# import functools
-# import logging
-# import os
-# import time
-
-# import aiohttp
-# import azure.functions as func
-
-
-# def timer(func):
-#     if asyncio.iscoroutinefunction(func):
-
-#         @functools.wraps(func)
-#         async def wrapper(*args, **kwargs):
-#             start_time = time.time()
-#             result = await func(*args, **kwargs)
-#             logging.info(
-#                 f"total execution time for async {func.__name__}: {time.time() - start_time}"
-#             )
-#             if result:
-#                 return result
-
-#         return wrapper
-#     else:
-
-#         @functools.wraps(func)
-#         def wrapper(*args, **kwargs):
-#             start_time = time.time()
-#             result = func(*args, **kwargs)
-#             logging.info(
-#                 f"total execution time for sync {func.__name__}: {time.time() - start_time}"
-#             )
-#             if result:
-#                 return result
-
-#         return wrapper
-
-
-# def get_api_headers_decorator(func):
-#     @functools.wraps(func)
-#     async def wrapper(session, *args, **kwargs):
-#         return {
-#             "Authorization": f"Basic {base64.b64encode(bytes(os.environ[args[0]], 'utf-8')).decode('utf-8')}"
-#             if "PAT" in args[0]
-#             else f"Bearer {os.environ[args[0]] if 'EA' in args[0] else await func(session, *args, **kwargs)}",
-#             "Content-Type": "application/json-patch+json"
-#             if "PAT" in args[0]
-#             else "application/json",
-#         }
-
-#     return wrapper
-
-
-# @get_api_headers_decorator
-# async def get_api_headers(session, *args, **kwargs):
-#     oauth2_headers = {"Content-Type": "application/x-www-form-urlencoded"}
-#     oauth2_body = {
-#         "client_id": os.environ[args[0]],
-#         "client_secret": os.environ[args[1]],
-#         "grant_type": "client_credentials",
-#         "scope" if "GRAPH" in args[0] else "resource": args[2],
-#     }
-#     async with session.post(
-#         url=args[3], headers=oauth2_headers, data=oauth2_body
-#     ) as resp:
-#         return (await resp.json())["access_token"]
-
-
-# @timer
-# async def main(mytimer: func.TimerRequest) -> None:
-#     logging.info("******* Starting main function *******")
-#     async with aiohttp.ClientSession() as session:
-#         graph_api_headers = next(
-#             iter(
-#                 await asyncio.gather(
-#                     *(
-#                         get_api_headers(session, *param)
-#                         for param in [
-#                             [
-#                                 "GRAPH_CLIENT_ID",
-#                                 "GRAPH_CLIENT_SECRET",
-#                                 "https://graph.microsoft.com/.default",
-#                                 f"https://login.microsoftonline.com/{os.environ['TENANT_ID']}/oauth2/v2.0/token",
-#                             ]
-#                         ]
-#                     )
-#                 )
-#             )
-#         )
-
+import asyncio
+import base64
+import functools
 import logging
+import os
+import time
 
+import aiohttp
 import azure.functions as func
 
 
-def main(myblob: func.InputStream):
+def timer(func):
+    if asyncio.iscoroutinefunction(func):
+
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = await func(*args, **kwargs)
+            logging.info(
+                f"total execution time for async {func.__name__}: {time.time() - start_time}"
+            )
+            if result:
+                return result
+
+        return wrapper
+    else:
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            logging.info(
+                f"total execution time for sync {func.__name__}: {time.time() - start_time}"
+            )
+            if result:
+                return result
+
+        return wrapper
+
+
+def get_api_headers_decorator(func):
+    @functools.wraps(func)
+    async def wrapper(session, *args, **kwargs):
+        return {
+            "Authorization": f"Basic {base64.b64encode(bytes(os.environ[args[0]], 'utf-8')).decode('utf-8')}"
+            if "PAT" in args[0]
+            else f"Bearer {os.environ[args[0]] if 'EA' in args[0] else await func(session, *args, **kwargs)}",
+            "Content-Type": "application/json-patch+json"
+            if "PAT" in args[0]
+            else "application/json",
+        }
+
+    return wrapper
+
+
+@get_api_headers_decorator
+async def get_api_headers(session, *args, **kwargs):
+    oauth2_headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    oauth2_body = {
+        "client_id": os.environ[args[0]],
+        "client_secret": os.environ[args[1]],
+        "grant_type": "client_credentials",
+        "scope" if "GRAPH" in args[0] else "resource": args[2],
+    }
+    async with session.post(
+        url=args[3], headers=oauth2_headers, data=oauth2_body
+    ) as resp:
+        return (await resp.json())["access_token"]
+
+
+@timer
+async def main(myblob: func.InputStream) -> None:
+    logging.info("******* Starting main function *******")
     logging.info(
         f"Python blob trigger function processed blob \n"
         f"Name: {myblob.name}\n"
-        f"Blob Size: {myblob.length} bytes"
+        f"Blob Size: {myblob.length} bytes\n"
+        f"Blob Uri: {myblob.uri}\n"
     )
+    async with aiohttp.ClientSession() as session:
+        graph_api_headers = next(
+            iter(
+                await asyncio.gather(
+                    *(
+                        get_api_headers(session, *param)
+                        for param in [
+                            [
+                                "GRAPH_CLIENT_ID",
+                                "GRAPH_CLIENT_SECRET",
+                                "https://graph.microsoft.com/.default",
+                                f"https://login.microsoftonline.com/{os.environ['TENANT_ID']}/oauth2/v2.0/token",
+                            ]
+                        ]
+                    )
+                )
+            )
+        )
+        async with session.put(
+            url=f"https://graph.microsoft.com/v1.0/drives/{os.environ['DRIVE_ID']}/root:/GoFex%20Reporting/{myblob.name.split('/')[-1]}:/content",
+            headers=graph_api_headers,
+            data=myblob.read(),
+        ) as resp:
+            logging.info(f"Response status code: {resp.status}")
+            logging.info(f"Response body: {await resp.json()}")
+
+    logging.info("******* Ending main function *******")
